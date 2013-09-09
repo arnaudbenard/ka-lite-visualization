9dd028ccbb1456d3d1893a0995685b2983461354
Arnaud Benard <arnaudbenard13@gmail.com>
2013-09-09 13:41:25 +0200

    Nested data by date

--- app.js
+++ app.js
@@ -5,41 +5,52 @@ App.controller('KALiteCtrl', function($scope, $http) {
 
 	$http.get('data.json')
 	.then(function(res){
-		$scope.devices = res.data;
+		$scope.data = organizeData(res.data);
 	});
 
 	$scope.options = {width: 500, height: 300, 'bar': 'aaa'};
-	$scope.data = [1, 2, 3, 4];
 });
 
-App.directive('barChart', function () {
+// App.directive('barChart', function () {
 
-	// The chart is defined in bar_chart.js
-	var chart = d3.custom.barChart();
-	return {
-		restrict: 'E',
-		replace: true,
-		template: '<div class="chart"></div>',
-		scope:{
-			height: '=height',
-			data: '=data',
-			hovered: '&hovered'
-		},
-		link: function(scope, element, attrs) {
-			var chartEl = d3.select(element[0]);
-			chart.on('customHover', function(d, i){
-				scope.hovered({args:d});
-			});
+// 	// The chart is defined in bar_chart.js
+// 	var chart = d3.custom.barChart();
+// 	return {
+// 		restrict: 'E',
+// 		replace: true,
+// 		template: '<div class="chart"></div>',
+// 		scope:{
+// 			height: '=height',
+// 			data: '=data',
+// 			hovered: '&hovered'
+// 		},
+// 		link: function(scope, element, attrs) {
+// 			var chartEl = d3.select(element[0]);
+// 			chart.on('customHover', function(d, i){
+// 				scope.hovered({args:d});
+// 			});
 
-			scope.$watch('data', function (newVal, oldVal) {
-				chartEl.datum(newVal).call(chart);
-			});
+// 			scope.$watch('data', function (newVal, oldVal) {
+// 				chartEl.datum(newVal).call(chart);
+// 			});
 
-			scope.$watch('height', function(d, i){
-				chartEl.call(chart.height(scope.height));
-			})
-		}
-	}
+// 			scope.$watch('height', function(d, i){
+// 				chartEl.call(chart.height(scope.height));
+// 			});
+// 		}
+// 	};
+// });
+
+function organizeData(raw_data){
 
 
-});
+	var nestedData = d3.nest()
+    .key(function(d) {
+		return  d.first_sess.substr(0, 10);
+    })
+    .sortKeys(d3.ascending)
+    .entries(raw_data);
+
+    console.log(nestedData);
+    return raw_data;
+}
