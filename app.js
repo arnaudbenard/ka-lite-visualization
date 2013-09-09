6b230f7298e443e1196bf11556a472000f648465
Arnaud Benard <arnaudbenard13@gmail.com>
2013-09-09 18:34:06 +0200

    Device registration in Json

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ App.controller('KALiteCtrl', function($scope, $http) {
 
 	$http.get('data.json')
 	.then(function(res){
-		//$scope.data = nestbyDate(res.data);
-		$scope.data = "1,2,3,4";
+		$scope.data = nestbyDate(res.data);
+		//$scope.data = "1 2 3 4";
 	});
 });
 
@@ -45,14 +45,45 @@ App.directive('chart', function ($parse) {
 
 function nestbyDate(raw_data){
 
+	// Constants
+	var firstDate = raw_data[0].first_sess,
+	//lastDate = raw_data[nModels-1].last_sess,
+	nModels=raw_data;
+
+	// Classify the data by date
 	var nestedData = d3.nest()
 	.key(function(d) {
 		return  d.first_sess.substr(0, 10); // Removes the THH:MM:SS"
 	})
 	.sortKeys(d3.ascending)
-	.entries(raw_data);
+	.entries(raw_data)
+	.map(function(d){
+		var date = d.key;
+		var total = 0;
 
-	console.log(nestedData);
+		//Evaluates each device with the same date
+		var values = d.values.forEach(function(dd){
+			total = total + 1;
+		});
+		return {'date':date, 'total':total};
+	});
+
+
+	obj = {};
+
+	// We will build the following model:
+	// [{'date':date, 'counter':counter},...]
+	// for(var i in nestedData){
+
+	// 	while (next_date < cur_date) {
+
+	// 		// Make sure to fill in empty rows for missing dates
+	// 		data.addRow([next_date.toLocaleDateString(), 0, ""]);
+	// 		next_date.setDate(next_date.getDate()+1);  // move on to the next day
+	// 	}
+
+	// }
 
 	return nestedData;
+
 }
